c77c22c8add26916051b7b94d0f59feeb86ae07e
Rene Ensle <rene.ensle@fthg.at>
2024-07-04 16:06:51 +0200

    #1: added base game loop with user input

---
+++ src/main/java/fht/project/TicTacToe.java
@@ -0,0 +1,65 @@
+package fht.project;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Board board;
+
+    // Constructor
+    public TicTacToe() {
+        this.player1 = new Player('x');
+        this.player2 = new Player('o');
+        this.board = new Board();
+
+        this.currentPlayer = player1;
+    }
+
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Current player: " + currentPlayer.getMarker());
+            int row = -1;
+
+            while (row < 0 || row > 2) {
+                if (sc.hasNextInt()) {
+                    row = sc.nextInt();
+                } else {
+                    sc.next();
+                }
+            }
+            System.out.println("row (0-2): " + row);
+
+            int column = -1;
+
+            while (column < 0 || column > 2) {
+                if (sc.hasNextInt()) {
+                    column = sc.nextInt();
+                } else {
+                    sc.next();
+                }
+            }
+            System.out.println("column (0-2): " + column);
+
+            if (board.isCellEmpty(row, column)) {
+                board.place(row, column, currentPlayer.getMarker());
+                switchCurrentPlayer();
+            } else {
+                System.out.println("Zelle ist belegt");
+            }
+
+        }
+    }
+
+    private void switchCurrentPlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+
+    }
+}
