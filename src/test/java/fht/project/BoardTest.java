15fb0dea5672ddff67faa07002e5ccd14b93a909
Rene Ensle <rene.ensle@fthg.at>
2024-07-04 21:08:48 +0200

    #9: added tests

--- src/test/java/fht/project/BoardTest.java
+++ src/test/java/fht/project/BoardTest.java
@@ -1,7 +1,49 @@
 package fht.project;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class BoardTest {
+public class BoardTest {
+    private Board board;
 
+    @BeforeEach
+    public void setUp() {
+        board = new Board();
+    }
+
+    @Test
+    public void testPlaceMarker() {
+        board.place(0, 0, 'x');
+        assertEquals('x', board.getCells()[0][0]);
+    }
+
+    @Test
+    public void testIsCellEmpty() {
+        assertTrue(board.isCellEmpty(0, 0));
+        board.place(0, 0, 'x');
+        assertFalse(board.isCellEmpty(0, 0));
+    }
+
+    @Test
+    public void testIsFull() {
+        char[][] fullBoard = {
+                {'x', 'o', 'x'},
+                {'x', 'x', 'o'},
+                {'o', 'x', 'o'}
+        };
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board.place(i, j, fullBoard[i][j]);
+            }
+        }
+        assertTrue(board.isFull());
+    }
+
+    @Test
+    public void testClear() {
+        board.place(0, 0, 'x');
+        board.clear();
+        assertTrue(board.isCellEmpty(0, 0));
+    }
 }
--- src/test/java/fht/project/TicTacToeTest.java
+++ src/test/java/fht/project/TicTacToeTest.java
@@ -3,13 +3,116 @@ package fht.project;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TicTacToeTest {
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TicTacToeTest {
+    private TicTacToe game;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
+        game = new TicTacToe();
     }
 
     @Test
-    public void switchCurrentPlayerTest() {
+    public void testInitialCurrentPlayer() throws Exception {
+        Field currentPlayerField = TicTacToe.class.getDeclaredField("currentPlayer");
+        currentPlayerField.setAccessible(true);
+        Player currentPlayer = (Player) currentPlayerField.get(game);
+        assertEquals('x', currentPlayer.getMarker());
+    }
+
+    @Test
+    public void testSwitchCurrentPlayer() throws Exception {
+        Method switchCurrentPlayerMethod = TicTacToe.class.getDeclaredMethod("switchCurrentPlayer");
+        switchCurrentPlayerMethod.setAccessible(true);
+        switchCurrentPlayerMethod.invoke(game);
+
+        Field currentPlayerField = TicTacToe.class.getDeclaredField("currentPlayer");
+        currentPlayerField.setAccessible(true);
+        Player currentPlayer = (Player) currentPlayerField.get(game);
+        assertEquals('o', currentPlayer.getMarker());
+    }
+
+    @Test
+    public void testHasWinnerRow() throws Exception {
+        Field boardField = TicTacToe.class.getDeclaredField("board");
+        boardField.setAccessible(true);
+        Board board = (Board) boardField.get(game);
+
+        board.place(0, 0, 'x');
+        board.place(0, 1, 'x');
+        board.place(0, 2, 'x');
+
+        Method hasWinnerMethod = TicTacToe.class.getDeclaredMethod("hasWinner");
+        hasWinnerMethod.setAccessible(true);
+        boolean hasWinner = (boolean) hasWinnerMethod.invoke(game);
+
+        assertTrue(hasWinner);
+    }
+
+    @Test
+    public void testHasWinnerColumn() throws Exception {
+        Field boardField = TicTacToe.class.getDeclaredField("board");
+        boardField.setAccessible(true);
+        Board board = (Board) boardField.get(game);
+
+        board.place(0, 0, 'o');
+        board.place(1, 0, 'o');
+        board.place(2, 0, 'o');
+
+        Method hasWinnerMethod = TicTacToe.class.getDeclaredMethod("hasWinner");
+        hasWinnerMethod.setAccessible(true);
+        boolean hasWinner = (boolean) hasWinnerMethod.invoke(game);
+
+        assertTrue(hasWinner);
+    }
+
+    @Test
+    public void testHasWinnerDiagonal() throws Exception {
+        Field boardField = TicTacToe.class.getDeclaredField("board");
+        boardField.setAccessible(true);
+        Board board = (Board) boardField.get(game);
+
+        board.place(0, 0, 'x');
+        board.place(1, 1, 'x');
+        board.place(2, 2, 'x');
+
+        Method hasWinnerMethod = TicTacToe.class.getDeclaredMethod("hasWinner");
+        hasWinnerMethod.setAccessible(true);
+        boolean hasWinner = (boolean) hasWinnerMethod.invoke(game);
+
+        assertTrue(hasWinner);
+    }
+
+    @Test
+    public void testIsDraw() throws Exception {
+        Field boardField = TicTacToe.class.getDeclaredField("board");
+        boardField.setAccessible(true);
+        Board board = (Board) boardField.get(game);
+
+        char[][] drawBoard = {
+                {'x', 'o', 'x'},
+                {'x', 'x', 'o'},
+                {'o', 'x', 'o'}
+        };
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board.place(i, j, drawBoard[i][j]);
+            }
+        }
+
+        Method isFullMethod = Board.class.getDeclaredMethod("isFull");
+        isFullMethod.setAccessible(true);
+        boolean isFull = (boolean) isFullMethod.invoke(board);
+
+        Method hasWinnerMethod = TicTacToe.class.getDeclaredMethod("hasWinner");
+        hasWinnerMethod.setAccessible(true);
+        boolean hasWinner = (boolean) hasWinnerMethod.invoke(game);
+
+        assertTrue(isFull);
+        assertFalse(hasWinner);
     }
 }
